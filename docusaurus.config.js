a6e2b30d3e60f96ffdd6da2d5fe1d5bf10befe68
Alan Pereira <alan_pereira@mckinsey.com>
2024-03-30 12:48:56 +0000

    feat: add GHA workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,19 +9,19 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Notepad',
-  tagline: 'My personal notepad',
+  tagline: 'A Person Notepad',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ansperson.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/notepad/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'MySelf', // Usually your GitHub org/user name.
+  projectName: 'Notepad', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
